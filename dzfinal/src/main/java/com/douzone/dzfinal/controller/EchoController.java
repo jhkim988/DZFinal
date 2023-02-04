29e3d5bcd644b32f6b0cc0e8a8c3b1cb9b1b775f
jhkim <jinhankim988@gmail.com>
2023-02-04 18:11:03 +0900

    [Chore] 젠킨스 배포환경 테스트

---
+++ dzfinal/src/main/java/com/douzone/dzfinal/controller/EchoController.java
@@ -0,0 +1,18 @@
+package com.douzone.dzfinal.controller;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class EchoController {
+	
+	@PostMapping("/echo")
+	public Map<String, Object> echo(@RequestBody Map<String, Object> map) {
+		return map;
+	}
+}
