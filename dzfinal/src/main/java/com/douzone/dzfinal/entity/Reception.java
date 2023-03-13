2f6a86866d0ae8d31573a520cf545800448fe36d
hyeeebin <knwsome@gmail.com>
2023-03-13 18:40:33 +0900

    [Feat]접수
    save

---
+++ dzfinal/src/main/java/com/douzone/dzfinal/entity/Reception.java
@@ -0,0 +1,17 @@
+package com.douzone.dzfinal.entity;
+
+import org.apache.ibatis.type.Alias;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Reception {
+	private String id;
+	private String pwd;
+}
