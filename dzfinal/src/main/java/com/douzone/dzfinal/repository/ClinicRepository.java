91e9ef035e87e6fd72d876c4ecaa5842c861b6e4
jhkim <jinhankim988@gmail.com>
2023-03-17 17:13:57 +0900

    [Fix] 잘못 연결된 DTO 수정 및 삭제

--- dzfinal/src/main/java/com/douzone/dzfinal/repository/ClinicRepository.java
+++ dzfinal/src/main/java/com/douzone/dzfinal/repository/ClinicRepository.java
@@ -3,19 +3,15 @@ package com.douzone.dzfinal.repository;
 import java.util.List;
 import java.util.Optional;
 
-import com.douzone.dzfinal.dto.PatientDrugJoin;
-import com.douzone.dzfinal.dto.PatientUnderlyingJoin;
 import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Repository;
 
 import com.douzone.dzfinal.dto.ClinicResponse;
-import com.douzone.dzfinal.entity.Disease;
-import com.douzone.dzfinal.entity.Drug;
 
 @Repository
 @Mapper
 public interface ClinicRepository {
 	Optional<ClinicResponse.Clinic> getClinic(int reception_id);
-	List<PatientDrugJoin> getDrugList(@Param("patient_id") int patient_id);
-	List<PatientUnderlyingJoin> getDiseaseList(@Param("patient_id") int patient_id);
+	List<ClinicResponse.DrugTaking> getDrugList(@Param("patient_id") int patient_id);
+	List<ClinicResponse.Underlying> getDiseaseList(@Param("patient_id") int patient_id);
 }
