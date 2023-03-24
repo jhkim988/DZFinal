86e569a1b1e9097feea74c97fba4c5c0a286f5d0
jhkim <jinhankim988@gmail.com>
2023-03-24 17:55:25 +0900

    [Rename] phone1 -> phone_number1

--- dzfinal/src/main/java/com/douzone/dzfinal/dto/PatientDTO.java
+++ dzfinal/src/main/java/com/douzone/dzfinal/dto/PatientDTO.java
@@ -11,8 +11,8 @@ public class PatientDTO {
     @Data
     @Builder
     public static class PhoneNumbers {
-        private String phone1;
-        private String phone2;
-        private String phone3;
+        private String phone_number1;
+        private String phone_number2;
+        private String phone_number3;
     }
 }
