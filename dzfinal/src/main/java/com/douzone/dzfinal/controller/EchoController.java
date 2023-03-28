b257c513b34295b77a7698f071b18f36ada8ce43
jhkim <jinhankim988@gmail.com>
2023-03-28 14:19:18 +0900

    [Feat] MQTT 샘플코드

--- dzfinal/src/main/java/com/douzone/dzfinal/controller/EchoController.java
+++ dzfinal/src/main/java/com/douzone/dzfinal/controller/EchoController.java
@@ -1,9 +1,11 @@
 package com.douzone.dzfinal.controller;
 
-import com.douzone.dzfinal.config.MqttOutboundGateway;
+import com.douzone.dzfinal.dto.WaitingDTO;
+import com.douzone.dzfinal.service.MqttOutboundGateway;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,8 +14,15 @@ public class EchoController {
 	@Autowired
 	private MqttOutboundGateway gateway;
 	@GetMapping("/test")
-	public void test() {
-		System.out.println("TEST Controller: " + "{\"method\":\"PUT\",\"data\":{\"reception_id\":\"7\",\"state\":\"수납완료\"}}");
-		gateway.sendToMqtt("{\"method\":\"PUT\",\"data\":{\"reception_id\":\"7\",\"state\":\"수납완료\"}}", "waiting", 1);
+	public void test() throws JsonProcessingException {
+		WaitingDTO waitingDTO = WaitingDTO.builder()
+				.method("PUT")
+				.data(WaitingDTO.WaitingData.builder()
+						.reception_id(7)
+						.state("수납완료")
+						.build())
+				.build();
+		ObjectMapper mapper = new ObjectMapper();
+		gateway.sendToMqtt(mapper.writeValueAsString(waitingDTO), "waiting", 1);
 	}
 }
