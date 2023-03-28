0538fd2e513767ba565095def80be5bcb3b96cbc
jhkim <jinhankim988@gmail.com>
2023-03-28 16:15:08 +0900

    [Feat] MQTT 샘플 코드

--- dzfinal/src/main/java/com/douzone/dzfinal/controller/EchoController.java
+++ dzfinal/src/main/java/com/douzone/dzfinal/controller/EchoController.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,12 +15,12 @@ public class EchoController {
 	@Autowired
 	private MqttOutboundGateway gateway;
 	@GetMapping("/test")
-	public void test() throws JsonProcessingException {
+	public void test(@RequestParam("reception_id") int reception_id, @RequestParam String state) throws JsonProcessingException {
 		WaitingDTO waitingDTO = WaitingDTO.builder()
 				.method("PUT")
 				.data(WaitingDTO.WaitingData.builder()
-						.reception_id(7)
-						.state("수납완료")
+						.reception_id(reception_id)
+						.state(state)
 						.build())
 				.build();
 		ObjectMapper mapper = new ObjectMapper();
