06aed694ba04245c4a9923fc9809c018628da0dd
이정주 <wjdwn3132@naver.com>
2023-03-30 19:47:34 +0900

    [Feat] did_setting 구현 중

--- dzfinal/src/main/java/com/douzone/dzfinal/controller/DidController.java
+++ dzfinal/src/main/java/com/douzone/dzfinal/controller/DidController.java
@@ -6,6 +6,7 @@ import java.io.OutputStream;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -60,5 +61,10 @@ public class DidController {
 		}
 		
 		return message;
+	}
+	
+	@DeleteMapping("/did_subtitle")
+	public void deleteMessage(@RequestParam("id") int id) {
+		didService.deleteMessgae(id);
 	}
 }
