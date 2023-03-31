aaf544a0ffeb4f602a4a978d6ba3333880c1447e
jhkim <jinhankim988@gmail.com>
2023-03-31 15:42:48 +0900

    [Feat] 대기열 mqtt 메세지 송신

--- dzfinal/src/main/java/com/douzone/dzfinal/service/MqttMessageService.java
+++ dzfinal/src/main/java/com/douzone/dzfinal/service/MqttMessageService.java
@@ -41,7 +41,6 @@ public class MqttMessageService {
     }
 
     public void updateReception(String message) {
-        System.out.println("Update Reception");
         try {
             WaitingDTO dto = mapper.readValue(message, WaitingDTO.class);
             WaitingDTO.WaitingData data = dto.getData();
