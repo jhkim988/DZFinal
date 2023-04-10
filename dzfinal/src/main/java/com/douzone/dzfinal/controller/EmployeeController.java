0044c1d929477e876b46c7cf4a1056969a6119e1
hyeeebin <knwsome@gmail.com>
2023-04-10 18:03:43 +0900

    [Feat]비밀번호 변경 토큰 user_name 얻기

--- dzfinal/src/main/java/com/douzone/dzfinal/controller/EmployeeController.java
+++ dzfinal/src/main/java/com/douzone/dzfinal/controller/EmployeeController.java
@@ -2,7 +2,15 @@ package com.douzone.dzfinal.controller;
 
 import com.douzone.dzfinal.dto.EmployeeDTO;
 import com.douzone.dzfinal.service.EmployeeService;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
+import org.springframework.security.oauth2.server.resource.authentication.JwtBearerTokenAuthenticationConverter;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,8 +19,17 @@ public class EmployeeController {
 
 	@Autowired
 	private EmployeeService employeeService;
+	
 	@GetMapping("/{user_id}")
 	public EmployeeDTO.EmployeeLoginInfo getEmployee(@PathVariable("user_id") String user_id) {
 		return employeeService.getEmployee(user_id);
+	}
+	
+	@GetMapping("/by_token")
+	public EmployeeDTO.EmployeeInfo selectEmployeeInfo(Authentication auth){
+		System.out.println(auth);
+		OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails) auth.getDetails();
+		System.out.println(details.getDecodedDetails());
+		return employeeService.selectEmployeeInfo("");
 	}
 }
--- dzfinal/src/main/java/com/douzone/dzfinal/dto/EmployeeDTO.java
+++ dzfinal/src/main/java/com/douzone/dzfinal/dto/EmployeeDTO.java
@@ -19,4 +19,20 @@ public class EmployeeDTO {
 //        private List<String> authority;
         private String authority;
     }
+    
+    @NoArgsConstructor
+	@AllArgsConstructor
+	@Data
+	@Builder
+	public static class EmployeeInfo {
+		private int employ_id;
+		private String user_id;
+		private String pwd;
+		private String employee_name;
+		private String birth;
+		private String role;
+		private String image_name;
+		private String real_image;
+		private String employee_email;
+	}
 }
