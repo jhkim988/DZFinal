45b204db1e22f0c75274ba0ae0c0bf30ada31c18
jhkim <jinhankim988@gmail.com>
2023-04-20 17:54:21 +0900

    [Refactor] no-offset 방식 pagination

--- dzfinal/src/main/java/com/douzone/dzfinal/repository/ChatRepository.java
+++ dzfinal/src/main/java/com/douzone/dzfinal/repository/ChatRepository.java
@@ -14,7 +14,7 @@ public interface ChatRepository {
 	List<ChatDTO.ChatRoom> chatRoomList(int participants_id);
 	List<ChatDTO.Thumbnail> getThumbnail();
 	void insert(ChatDTO.Message chatMessage);
-	List<ChatDTO.Message> getChatRoomMessages(@Param("chatroom_id") int participants_id, @Param("page") int page);
+	List<ChatDTO.Message> getChatRoomMessages(@Param("chatroom_id") int participants_id, @Param("last") int lastChatId);
 	List<ChatDTO.MessageCount> getMessageCount(int participants_id);
 	List<Integer> getNotificationTargetIds(@Param("chatroom_id") int chatroom_id, @Param("participants_id") int participants_id);
 	void updateLastReadTime(ChatDTO.ChatRoomStatus status);
